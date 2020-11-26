40ebd0e9d2df1cd4dacdf766547ac6e830cbdd0e
Navid Shaikh <nshaikh@redhat.com>
2020-11-26 19:25:25 +0530

    Update hack/update-codegen.sh

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -19,17 +19,6 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
-
-# generate the code with:
-# --output-base    because this script should also be able to run inside the vendor dir of
-#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-#                  instead of the $GOPATH directly. For normal projects this can be dropped.
-bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy" \
-  knative.dev/client/pkg/apis/client/v1alpha1/generated knative.dev/client/pkg/apis \
-  client:v1alpha1 \
-  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
-  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
 
 # Make sure our dependencies are up-to-date
 ${SCRIPT_ROOT}/hack/update-deps.sh
